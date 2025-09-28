97c199efc5e01895ea448e727a7896fba6493846
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-09-28 14:32:43 +0200

    deploying backend

--- frontend/src/app/services/content.ts
+++ frontend/src/app/services/content.ts
@@ -3,12 +3,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Content } from '../models/content';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
-  private readonly apiUrl = 'https://daily-spark-vuba.onrender.com/api';
+    private readonly apiUrl = environment.apiUrl;
+
 
   constructor(private readonly http: HttpClient) { }
 
---
+++ frontend/src/env.js
@@ -0,0 +1,3 @@
+window.env = {
+    API_URL: process.env.API_URL
+};
